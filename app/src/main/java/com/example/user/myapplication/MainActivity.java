12039e56ecba4a05c448dc7ad70cd79597f05773
Shiuan Kai <t105360045@ntut.org.tw>
2018-10-09 22:29:16 +0900

    還原到LAB2

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
